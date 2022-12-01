167316f7984e5bf61e6a4f3a89206b7e55486e31
Guilherme <carvalho_guilhermegomes@hotmail.com>
2022-12-01 11:27:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./style/search.css";
+import lupa from "./assets/lupa.svg";
+import Like from "./assets/like.svg";
+import del from "./assets/delete.svg";
+import list from "./searchList";
+import logo from "./assets/logo.png";
+import views from "./assets/views.svg";
+import edit from "./assets/edit.svg";
+
 
 function App() {
+  const [input, setInput] = useState("");
+  const [filterImg, setFilterImg] = useState(list);
+
+  function handleSearch() {
+    const filterImgToSave = list.filter((post) =>
+      post.name.toLowerCase().includes(input.toLowerCase())
+    );
+    setFilterImg(filterImgToSave);
+    setInput('');
+  }
+
+  console.log("*input*", input);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <div className="headerContent">
+          <img src={logo} />
+          <div className="searchContainer">
+            <img src={lupa} className="lupa" onClick={handleSearch} />
+            <input
+              className="search"
+              placeholder="Buscar"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+            />
+          </div>
+        </div>
       </header>
+
+        <div className="container">
+          {filterImg.map((lista) => {
+            return (
+              <div className="content" key={lista.id}>
+                <div className='containerImgIcons'>
+                <div className='icons'>
+                  <img src={edit} />
+                  <img src={del} />
+                </div>
+                <img className="img" src={lista.url} />
+                </div>
+                <div className="info">
+                  <h1>{lista.name}</h1>
+                  <div className="data">
+                    <div className='containerData'>
+                      <img className="like" src={Like} />
+                      <strong>{lista.likes}mil</strong>
+                    </div>
+                    <div className='containerData'>
+                      <img className="views" src={views} />
+                      <strong>{lista.views}mil</strong>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+                  <button className="myButton">+</button>
+        </div>
+
     </div>
   );
 }
---
+++ src/searchList.js
@@ -0,0 +1,76 @@
+const arrListImg = [
+    {
+      id: 1,
+      name: "Elefante",
+      likes: 2,
+      views: 16,
+      url:
+        "https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_960_720.jpg"
+    },
+    {
+      id: 2,
+      name: "Raposa",
+      likes: 1,
+      views: 23,
+      url:
+        "https://cdn.pixabay.com/photo/2016/12/05/11/39/fox-1883658_960_720.jpg"
+    },
+    {
+      id: 3,
+      name: "Céu Estrelado",
+      likes: 233,
+      views: 55,
+      url:
+        "https://cdn.pixabay.com/photo/2017/03/28/22/55/night-photograph-2183637_960_720.jpg"
+    },
+    {
+      id: 4,
+      name: "Edifícios",
+      likes: 455,
+      views: 23,
+      url:
+        "https://cdn.pixabay.com/photo/2017/03/28/22/55/night-photograph-2183637_960_720.jpg"
+    },
+    {
+      id: 5,
+      name: "Fotógrafo",
+      likes: 10,
+      views: 20,
+      url:
+        "https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010_960_720.jpg"
+    },
+    {
+      id: 6,
+      name: "Framboesas",
+      likes: 3,
+      views: 55,
+      url:
+        "https://cdn.pixabay.com/photo/2017/01/31/09/30/raspberries-2023404_960_720.jpg"
+    },
+    {
+      id: 7,
+      name: "Águia",
+      likes: 3500,
+      views: 56,
+      url:
+        "https://cdn.pixabay.com/photo/2017/06/09/09/39/adler-2386314_960_720.jpg"
+    },
+    {
+      id: 8,
+      name: "Leite com Morango",
+      likes: 2300,
+      views: 45,
+      url:
+        "https://cdn.pixabay.com/photo/2020/06/06/16/40/milk-5267300_960_720.jpg"
+    },
+    {
+      id: 9,
+      name: "Anjo",
+      likes: 3500,
+      views: 10,
+      url:
+        "https://cdn.pixabay.com/photo/2020/06/06/16/40/milk-5267300_960_720.jpg"
+    }
+  ]
+
+  export default arrListImg;
