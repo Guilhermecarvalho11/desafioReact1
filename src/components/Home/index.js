ab353677b6b9374397c7468f9313717f928ece44
Guilherme Carvalho <guilherme.gomes@afya.com.br>
2022-12-15 13:14:57 -0300

    adicionando descricoes de acessibilidade nas imagens

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../../style/search.css";
 import lupa from "../../assets/lupa.svg";
 import Like from "../../assets/like.svg";
